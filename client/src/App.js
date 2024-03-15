b55bd4d4a1ccfa3e5f495e389a21d08c2864f7d3
Manuel Schnidrig <manuel.schnidrig@ordergroove.com>
2024-03-15 13:25:10 -0300

    Basic app

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [customer, setCustomer] = useState({
+    firstName: '',
+    lastName: '',
+    email: ''
+  });
+  const [ws, setWs] = useState(new WebSocket('ws://localhost:8080'));
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCustomer(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    ws.send(JSON.stringify(customer));
+    alert('Customer data sent');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="firstName"
+            value={customer.firstName}
+            onChange={handleChange}
+            placeholder="First Name"
+            required
+          />
+          <input
+            type="text"
+            name="lastName"
+            value={customer.lastName}
+            onChange={handleChange}
+            placeholder="Last Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            value={customer.email}
+            onChange={handleChange}
+            placeholder="Email"
+            required
+          />
+          <button type="submit">Send Customer Data</button>
+        </form>
       </header>
     </div>
   );
