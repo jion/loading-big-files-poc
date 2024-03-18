273b8376dfcfb04ee180243431150f449208f22d
Manuel Schnidrig <manuel.schnidrig@ordergroove.com>
2024-03-17 21:40:51 -0300

    Not using self in worker

--- client/src/workers/dexie_upload.worker.js
+++ client/src/workers/dexie_upload.worker.js
@@ -1,31 +1,27 @@
-/* eslint-disable-next-line no-restricted-globals */
-self.onmessage = async function(e) {
+onmessage = async function(e) {
     const { file } = e.data;
     const reader = new FileReader();
 
     reader.onload = function() {
         const lines = reader.result.split('\n');
         for (const line of lines) {
-        try {
-            if (line) { // Avoid empty lines
-            const customer = JSON.parse(line);
-            // Post each customer object back to the main thread
-            /* eslint-disable-next-line no-restricted-globals */
-            self.postMessage({ customer });
+            try {
+                if (line) { // Avoid empty lines
+                    const customer = JSON.parse(line);
+                    // Post each customer object back to the main thread
+                    postMessage({ customer });
+                }
+            } catch (error) {
+                console.error('Error parsing JSON line in worker:', error);
+                // Optionally, send error back to main thread
+                postMessage({ error: error.toString() });
             }
-        } catch (error) {
-            console.error('Error parsing JSON line in worker:', error);
-            // Optionally, send error back to main thread
-            /* eslint-disable-next-line no-restricted-globals */
-            self.postMessage({ error: error.toString() });
-        }
         }
     };
 
     reader.onerror = function(error) {
         console.error('FileReader error:', error);
-        /* eslint-disable-next-line no-restricted-globals */
-        self.postMessage({ error: error.toString() });
+        postMessage({ error: error.toString() });
     };
 
     reader.readAsText(file);
