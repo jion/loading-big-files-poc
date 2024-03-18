860b07dd88d3b191ee61d0987e77434754941d90
Manuel Schnidrig <manuel.schnidrig@ordergroove.com>
2024-03-17 21:34:03 -0300

    Using workers from src

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,28 @@ import { useDropzone } from 'react-dropzone';
 import './WebSocketSyncProtocol';
 import { db } from './db';
 
-// Assuming your Web Worker script is named "worker.js" and located in the public folder
-const worker = new Worker('worker.js');
-
 db.syncable.connect('websocket', 'ws://localhost:8080');
 db.syncable.on('statusChanged', (newStatus, url) => {
   console.log(`Sync Status: ${Dexie.Syncable.StatusTexts[newStatus]}`);
 });
 
 function CustomerDataComponent() {
-  const [newCustomer, setNewCustomer] = useState({
-    firstName: '',
-    lastName: '',
-    email: ''
-  });
-
+  // Summary -------------------------------------------------------------------
   const customers = useLiveQuery(() => db.customers.toArray(), []);
 
-  const onDrop = useCallback(acceptedFiles => {
-    // Send file to web worker for processing
-    const file = acceptedFiles[0];
-    worker.postMessage({ file });
-  }, []);
+  const handleDelete = async (id) => {
+    await db.customers.delete(id);
+  };
+
+  // Worker --------------------------------------------------------------------
+  const [worker, setWorker] = useState();
 
   useEffect(() => {
+    // Initialize the web worker
+    const worker = new Worker(new URL('workers/dexie_upload.worker.js', import.meta.url), { type: 'module' });
+    console.log('Worker created:', worker);
+    setWorker(worker);
+
     // Listen for messages from the worker
     const handleMessage = (event) => {
       const { customer, error } = event.data;
@@ -53,24 +51,16 @@ function CustomerDataComponent() {
     };
   }, []);
 
+  // Dropzone ------------------------------------------------------------------
+  const onDrop = useCallback(acceptedFiles => {
+    // Send file to web worker for processing
+    const file = acceptedFiles[0];
+    worker.postMessage({ file });
+  }, [worker]);
+
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewCustomer(prevState => ({ ...prevState, [name]: value }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log('newCustomer:', newCustomer);
-    await db.customers.add(newCustomer);
-    setNewCustomer({ firstName: '', lastName: '', email: '' }); // Reset form
-  };
-
-  const handleDelete = async (id) => {
-    await db.customers.delete(id);
-  };
-
+  // React Component -----------------------------------------------------------
   return (
     <div>
       <div {...getRootProps()} style={{ border: '2px dashed #007bff', padding: '20px', marginBottom: '20px' }}>
--- client/src/workers/dexie_upload.worker.js
+++ client/src/workers/dexie_upload.worker.js
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line no-restricted-globals */
 self.onmessage = async function(e) {
     const { file } = e.data;
     const reader = new FileReader();
@@ -9,11 +10,13 @@ self.onmessage = async function(e) {
             if (line) { // Avoid empty lines
             const customer = JSON.parse(line);
             // Post each customer object back to the main thread
+            /* eslint-disable-next-line no-restricted-globals */
             self.postMessage({ customer });
             }
         } catch (error) {
             console.error('Error parsing JSON line in worker:', error);
             // Optionally, send error back to main thread
+            /* eslint-disable-next-line no-restricted-globals */
             self.postMessage({ error: error.toString() });
         }
         }
@@ -21,6 +24,7 @@ self.onmessage = async function(e) {
 
     reader.onerror = function(error) {
         console.error('FileReader error:', error);
+        /* eslint-disable-next-line no-restricted-globals */
         self.postMessage({ error: error.toString() });
     };
 
